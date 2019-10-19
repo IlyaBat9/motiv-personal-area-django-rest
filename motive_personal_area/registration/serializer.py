d2cefff47b6dfe6d01c3e72b297c473a9aa2a6e1
Ilya Solomein <ilya.solomein@gmail.com>
2019-10-19 20:19:32 +0500

    imports

--- motive_personal_area/registration/serializer.py
+++ motive_personal_area/registration/serializer.py
@@ -1,7 +1,4 @@
 from rest_framework import serializers
-from django.utils import timezone
-from re import match
-from django.contrib.auth import get_user_model
 
 
 class LoginSerializer(serializers.Serializer):
