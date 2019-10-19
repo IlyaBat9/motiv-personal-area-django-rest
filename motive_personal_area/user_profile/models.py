5f2be1e4ae66aad95fdd923fbea25a33267c5fcd
Ivan40962048 <flash.vanya@gmail.com>
2019-10-19 18:38:12 +0300

    Создал баланс в бд и убрал поле с номером телефона

--- motive_personal_area/user_profile/models.py
+++ motive_personal_area/user_profile/models.py
@@ -1,13 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from balance_info.models import Balance
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     middle_name = models.CharField(max_length=50, null=True)
-    phone = models.IntegerField(null=False)
+    balance = models.OneToOneField(Balance, on_delete=models.CASCADE, default=None)
 
     def get_full_name(self):
         if self.middle_name in None:
