38f13af712a4cac3109b79b47484b67ea6d39f90
Ilya Solomein <ilya.solomein@gmail.com>
2019-10-19 20:56:14 +0500

    serializer

--- motive_personal_area/user_profile/serializer.py
+++ motive_personal_area/user_profile/serializer.py
@@ -1,2 +1,18 @@
 from rest_framework import serializers
+from .models import Profile
+
+
+class UserBaseInfoSerializer(serializers.ModelSerializer):
+    def to_representation(self, instance):
+        return {
+            "number": instance.user.username,
+            "full_name": instance.get_full_name(),
+            "first_name": instance.first_name,
+            "last_name": instance.second_name,
+            "middle_name": instance.middle_name,
+        }
+
+    class Meta:
+        model = Profile
+        fields = "__all__"
 
